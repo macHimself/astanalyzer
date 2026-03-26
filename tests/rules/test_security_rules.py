f62f6e9ed87f4b8119f8189bc8e53306e3fa029a
Adam <54817317+macHimself@users.noreply.github.com>
2026-03-26 23:31:41 +0100

    test: expand security and resource rule coverage

--- tests/rules/test_security_rules.py
+++ tests/rules/test_security_rules.py
@@ -1,23 +1,117 @@
-from astanalyzer.rules.security import UseOfEval, OpenWithoutWith
+def test_use_of_eval_matches(scan_rule_ids):
+    rule_ids = scan_rule_ids(
+        "eval('1 + 1')\n",
+    )
+
+    assert "SEC-030" in rule_ids
 
 
-def test_use_of_eval_matches(parse_code):
-    tree = parse_code("eval('1 + 1')\n")
-    expr = next(tree.get_children())
-    call = expr.value
+def test_exec_matches(scan_rule_ids):
+    rule_ids = scan_rule_ids(
+        "exec('x = 1')\n",
+    )
 
-    rule = UseOfEval()
-    matches = rule.match_node(call, ctx={})
-
-    assert len(matches) == 1
+    assert "SEC-030" in rule_ids
 
 
-def test_open_without_with_matches(parse_code):
-    tree = parse_code("f = open('x.txt')\n")
-    assign = next(tree.get_children())
-    call = assign.value
+def test_eval_literal_parsing_candidate_matches(scan_rule_ids):
+    rule_ids = scan_rule_ids(
+        "eval('[1, 2, 3]')\n",
+    )
 
-    rule = OpenWithoutWith()
-    matches = rule.match_node(call, ctx={})
+    assert "SEC-035" in rule_ids
 
-    assert len(matches) == 1
+
+def test_eval_literal_parsing_candidate_not_detected_for_variable_input(scan_rule_ids):
+    rule_ids = scan_rule_ids(
+        "data = input()\n"
+        "eval(data)\n",
+    )
+
+    assert "SEC-035" not in rule_ids
+
+
+def test_use_of_os_system_matches(scan_rule_ids):
+    rule_ids = scan_rule_ids(
+        "import os\n"
+        "os.system('ls')\n",
+    )
+
+    assert "SEC-031" in rule_ids
+
+
+def test_use_of_os_popen_matches(scan_rule_ids):
+    rule_ids = scan_rule_ids(
+        "import os\n"
+        "os.popen('ls')\n",
+    )
+
+    assert "SEC-031" in rule_ids
+
+
+def test_hardcoded_password_matches(scan_rule_ids):
+    rule_ids = scan_rule_ids(
+        "password = 'secret123'\n",
+    )
+
+    assert "SEC-033" in rule_ids
+
+
+def test_hardcoded_token_matches(scan_rule_ids):
+    rule_ids = scan_rule_ids(
+        "api_token = 'abc123'\n",
+    )
+
+    assert "SEC-033" in rule_ids
+
+
+def test_hardcoded_password_not_detected_for_non_string(scan_rule_ids):
+    rule_ids = scan_rule_ids(
+        "password = 12345\n",
+    )
+
+    assert "SEC-033" not in rule_ids
+
+
+def test_insecure_random_matches(scan_rule_ids):
+    rule_ids = scan_rule_ids(
+        "import random\n"
+        "x = random.randint(1, 10)\n",
+    )
+
+    assert "SEC-034" in rule_ids
+
+
+def test_insecure_random_choice_matches(scan_rule_ids):
+    rule_ids = scan_rule_ids(
+        "import random\n"
+        "x = random.choice([1, 2, 3])\n",
+    )
+
+    assert "SEC-034" in rule_ids
+
+
+def test_insecure_random_not_detected_for_secrets(scan_rule_ids):
+    rule_ids = scan_rule_ids(
+        "import secrets\n"
+        "x = secrets.randbelow(10)\n",
+    )
+
+    assert "SEC-034" not in rule_ids
+
+
+def test_open_without_with_matches(scan_rule_ids):
+    rule_ids = scan_rule_ids(
+        "f = open('x.txt')\n",
+    )
+
+    assert "RES-032" in rule_ids
+
+
+def test_open_inside_with_not_detected(scan_rule_ids):
+    rule_ids = scan_rule_ids(
+        "with open('x.txt') as f:\n"
+        "    data = f.read()\n",
+    )
+
+    assert "RES-032" not in rule_ids
